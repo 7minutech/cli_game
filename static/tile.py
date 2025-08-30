fb73434cb9e0b49fe085f430cf1370c73d1995a1
7minutech <elijohnson1107@gmail.com>
2025-08-30 18:41:57 -0500

    feat: str repr methods and must not use mutable [] as default value for neighbors

--- static/tile.py
+++ static/tile.py
@@ -1,6 +1,15 @@
 class Tile:
     
-    def __init__(self, position, neigbors=None, owner=None):
+    def __init__(self, position, neighbors=None, owner=None):
         self.position = position
-        self.neigbors = neigbors
+        if neighbors is None:
+            self.neighbors = []
+        else:
+            self.neighbors = neighbors
         self.owner = owner
+    
+    def __str__(self):
+        return(f"Position: {self.position}, Owner: {self.owner}")
+    
+    def __repr__(self):
+        return(f"Tile(position:{self.position}, owner:{self.owner})")
